da637a5cd85e95ca1f1849f9ff874b1f94abd72c
Austin Murdock <austin.d.murdock@gmail.com>
2017-02-01 20:56:35 -0600

    Update: adding requirements to deploy on firebase

--- gulpfile.js
+++ gulpfile.js
@@ -10,164 +10,97 @@
 
 'use strict';
 
-const path = require('path');
+const del = require('del');
 const gulp = require('gulp');
 const gulpif = require('gulp-if');
-const filter = require('gulp-filter');
-
+const imagemin = require('gulp-imagemin');
+const mergeStream = require('merge-stream');
+const polymerBuild = require('polymer-build');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
-const postcss  = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const stylelint = require('stylelint');
-const cleanCSS = require('postcss-clean');
-const posthtml  = require('gulp-posthtml');
-const posthtmlPostcss = require('posthtml-postcss');
-const htmlmin = require('posthtml-minifier');
 
-// TODO: Re-enable if css is broken out properly by polymer-build
-// const cssSlam = require('css-slam').gulp;
-// const htmlMinifier = require('gulp-html-minifier');
+const swPrecacheConfig = require('./sw-precache-config.js');
+const polymerJson = require('./polymer.json');
+const polymerProject = new polymerBuild.PolymerProject(polymerJson);
+const buildDirectory = 'build';
 
-// Got problems? Try logging 'em
-// const logging = require('plylog');
-// logging.setVerbose();
-
-// !!! IMPORTANT !!! //
-// Keep the global.config above any of the gulp-tasks that depend on it
-global.config = {
-  polymerJsonPath: path.join(process.cwd(), 'polymer.json'),
-  build: {
-    rootDirectory: 'build',
-    bundledDirectory: 'bundled',
-    unbundledDirectory: 'unbundled',
-    // Accepts either 'bundled', 'unbundled', or 'both'
-    // A bundled version will be vulcanized and sharded. An unbundled version
-    // will not have its files combined (this is for projects using HTTP/2
-    // server push). Using the 'both' option will create two output projects,
-    // one for bundled and one for unbundled
-    bundleType: 'bundled'
-  },
-  // Path to your service worker, relative to the build root directory
-  serviceWorkerPath: 'service-worker.js',
-  // Service Worker precache options based on
-  // https://github.com/GoogleChrome/sw-precache#options-parameter
-  swPrecacheConfig: {
-    navigateFallback: '/index.html'
-  }
-};
-
-// Add your own custom gulp tasks to the gulp-tasks directory
-// A few sample tasks are provided for you
-// A task should return either a WriteableStream or a Promise
-const clean = require('./gulp-tasks/clean.js');
-const project = require('./gulp-tasks/project.js');
-const war = require('./gulp-tasks/war.js');
-
-// Filters
-const jsFilter = filter('**/*.js', {restore: true});
-const cssFilter = filter('**/*.css', {restore: true});
-const htmlFilter = filter('**/*.html', {restore: true});
-const jsDepsFilter = filter('**/*.js', {restore: true});
-const cssDepsFilter = filter('**/*.css', {restore: true});
-const htmlDepsFilter = filter('**/*.html', {restore: true});
-
-// Config
-const cssOptions = {};
-
-// The source task will split all of your source files into one
-// big ReadableStream. Source files are those in src/** as well as anything
-// added to the sourceGlobs property of polymer.json.
-// Because most HTML Imports contain inline CSS and JS, those inline resources
-// will be split out into temporary files. You can use gulpif to filter files
-// out of the stream and run them through specific tasks. An example is provided
-// which filters all images and runs them through imagemin
-function source() {
-  return project.splitSource()
-    // JS
-    .pipe(jsFilter)
-    .pipe(babel({
-      presets: ["es2015-script"],
-      plugins: ["array-includes"]
-    }))
-    .pipe(uglify())
-    .pipe(jsFilter.restore)
-
-    // CSS
-    .pipe(cssFilter)
-    .pipe(postcss([
-      // stylelint(),
-      autoprefixer(),
-      cleanCSS(cssOptions)
-    ]))
-    .pipe(cssFilter.restore)
-
-    //HTML
-    .pipe(htmlFilter)
-    .pipe(posthtml([
-      posthtmlPostcss([
-        // stylelint({}),
-        autoprefixer(),
-        cleanCSS(cssOptions)
-      ]),
-      htmlmin({
-        collapseWhitespace: true,
-        removeComments: true
-      })
-    ]))
-    .pipe(htmlFilter.restore)
-
-    .pipe(project.rejoin()); // Call rejoin when you're finished
+/**
+ * Waits for the given ReadableStream
+ */
+function waitFor(stream) {
+  return new Promise((resolve, reject) => {
+    stream.on('end', resolve);
+    stream.on('error', reject);
+  });
 }
 
-// The dependencies task will split all of your bower_components files into one
-// big ReadableStream
-// You probably don't need to do anything to your dependencies but it's here in
-// case you need it :)
-function dependencies() {
-  return project.splitDependencies()
-    // JS
-    .pipe(jsDepsFilter)
-    .pipe(babel())
-    .pipe(uglify())
-    .pipe(jsDepsFilter.restore)
+function build() {
+  return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
+    // Okay, so first thing we do is clear the build directory
+    console.log(`Deleting ${buildDirectory} directory...`);
+    del([buildDirectory])
+      .then(() => {
+        // Okay, now let's get your source files
+        let sourcesStream = polymerProject.sources()
+          // Oh, well do you want to minify stuff? Go for it!
+          // Here's how splitHtml & gulpif work
+          .pipe(polymerProject.splitHtml())
+          .pipe(gulpif(/\.js$/, babel({
+            presets: ["es2015-script"],
+            plugins: ["array-includes"]
+          })))
+          .pipe(gulpif(/\.js$/, uglify()))
+          // .pipe(gulpif(/\.css$/, cssSlam()))
+          // .pipe(gulpif(/\.html$/, htmlMinifier()))
+          .pipe(gulpif(/\.(png|gif|jpg|svg)$/, imagemin()))
+          .pipe(polymerProject.rejoinHtml());
 
-    // CSS
-    .pipe(cssDepsFilter)
-    .pipe(postcss([
-      autoprefixer(),
-      cleanCSS(cssOptions)
-    ]))
-    .pipe(cssDepsFilter.restore)
+        // Okay, now let's do the same to your dependencies
+        let dependenciesStream = polymerProject.dependencies()
+          .pipe(polymerProject.splitHtml())
+          .pipe(gulpif(/\.js$/, babel({
+            presets: ["es2015-script"],
+            plugins: ["array-includes"]
+          })))
+          .pipe(gulpif(/\.js$/, uglify()))
+          // .pipe(gulpif(/\.css$/, cssSlam()))
+          // .pipe(gulpif(/\.html$/, htmlMinifier()))
+          .pipe(polymerProject.rejoinHtml());
+          console.log('dependenciesStream', dependenciesStream.bundler);
 
-    //HTML
-    .pipe(htmlDepsFilter)
-    .pipe(posthtml([
-      posthtmlPostcss([
-        autoprefixer(),
-        cleanCSS(cssOptions)
-      ]),
-      htmlmin({
-        collapseWhitespace: true,
-        removeComments: true
+        // Okay, now let's merge them into a single build stream
+        let buildStream = mergeStream(sourcesStream, dependenciesStream)
+          .once('data', () => {
+            console.log('Analyzing build dependencies...');
+          });
+
+        // If you want bundling, pass the stream to polymerProject.bundler.
+        // This will bundle dependencies into your fragments so you can lazy
+        // load them.
+        buildStream = buildStream.pipe(polymerProject.bundler);
+
+        // Okay, time to pipe to the build directory
+        buildStream = buildStream.pipe(gulp.dest(buildDirectory));
+
+        // waitFor the buildStream to complete
+        return waitFor(buildStream);
       })
-    ]))
-    .pipe(htmlDepsFilter.restore)
-    .pipe(project.rejoin());
+      .then(() => {
+        // Okay, now let's generate the Service Worker
+        console.log('Generating the Service Worker...');
+        return polymerBuild.addServiceWorker({
+          project: polymerProject,
+          buildRoot: buildDirectory,
+          bundled: true,
+          swPrecacheConfig
+        });
+      })
+      .then(() => {
+        // You did it!
+        console.log('Build complete!');
+        resolve();
+      });
+  });
 }
 
-// Clean the build directory, split all source and dependency files into streams
-// and process them, and output bundled and unbundled versions of the project
-// with their own service workers
-gulp.task('default', gulp.series([
-  clean([global.config.build.rootDirectory]),
-  project.merge(source, dependencies),
-  project.serviceWorker
-]));
-
-gulp.task('war', gulp.series([
-  clean([global.config.build.rootDirectory]),
-  project.merge(source, dependencies),
-  project.serviceWorker,
-  war.create
-]));
+gulp.task('build', build);
